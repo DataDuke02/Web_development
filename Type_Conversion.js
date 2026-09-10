ca13ef90008dc44b2a9132cf56b9c92878217393
DataDuke02 <thirugnanasambantham2802@gmail.com>
2026-09-10 23:59:42 +0530

    day-11 of learning HTML, CSS and Day-3 of Learning Javascript

---
+++ Type_Conversion.js
@@ -0,0 +1,22 @@
+// Type Conversion = change the datatype of a value to another
+//                   (Strings, numbers, booleans)
+
+/*
+let age = window.prompt("How old are You?");
+age = Number(age);
+age+=1;
+
+console.log(age, typeof age);
+*/
+
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
