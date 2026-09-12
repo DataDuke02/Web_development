fa4e4d9e63406ee7d423a95c5c394f06bdab1498
DataDuke02 <thirugnanasambantham2802@gmail.com>
2026-09-12 07:29:55 +0530

    day-13 of learning HTML, CSS and Day-4 of Learning Javascript

---
+++ Math.js
@@ -0,0 +1,27 @@
+//  Math =  built-in object that provides a
+//          collection of properties and methods
+
+//console.log(Math.PI);
+//console.log(Math.E);
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+//z = Math.round(x);  //3.21 = 3
+//z = Math.floor(x);  //3.99 = 4
+//z = Math.ceil(x);   //3.21 = 4
+//z = Math.trunc(x);    //3.21  = 3
+//z = Math.Pow(x, y);     // 3^2 = 9
+//z = Math.sqrt(x);       // 81 = 9
+//z = Math.log(x);        // 10 = 2.3
+//z = Math.sin(x);        //45 = 0.8
+//z = Math.cos(x);        //45 = 0.52
+//z = Math.tan(x);        // 45 = 1.61
+//z = Math.abs(x);        // 3.21 =  +3.21
+//z = Math.sign(x);       // -3.21 = -1 0 = 0
+let max = Math.max(x, y, z);     // 3
+let min = Math.min(x, y, z);     // 1
+
+console.log(max);   
+console.log(min);
