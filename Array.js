705d4b5a05b30d291e3b7f39fac5f265e39f9f2e
DataDuke02 <thirugnanasambantham2802@gmail.com>
2026-09-25 00:48:14 +0530

    day-18 of learning HTML, CSS and Day-9 of Learning Javascript Array

---
+++ Array.js
@@ -0,0 +1,15 @@
+// Array = a variable like structure that can hold
+//         more than 1 value
+
+let fruits = ["apple", "orange", "banana"];
+
+fruits[3] = "cocount";
+fruits.push("Strawberry");
+//fruits.pop();
+
+console.log(fruits);
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
